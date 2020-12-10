e93363a1f84677eed2492e0cbb5f14dd7a77fe5a
Ailyan <ailyan.contact@gmail.com>
2020-12-10 14:57:49 +0100

    -comm

--- app/src/main/java/com/ailyan/androidnotifications/Notifications.java
+++ app/src/main/java/com/ailyan/androidnotifications/Notifications.java
@@ -1,4 +1,21 @@
 package com.ailyan.androidnotifications;
 
-public class Notifications {
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+class Notifications {
+    public static void infoDialog(Context ctx, String title, String message) {
+        AlertDialog.Builder b = new AlertDialog.Builder(ctx);
+        b.setMessage(message);
+        b.setCancelable(false);
+        b.setNeutralButton("Ok", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        b.setTitle(title);
+        AlertDialog ad = b.create();
+        ad.show();
+    }
 }
